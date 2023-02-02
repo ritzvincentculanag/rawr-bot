023478f484a9e25ca5fbad133eb47eb6a65520f2
RexcellentRawr <rexcellent.rawr@gmail.com>
2023-02-02 21:51:31 +0800

    add initial Interaction feature

---
+++ src/features/interaction.py
@@ -0,0 +1,30 @@
+from discord.app_commands.commands import command
+from discord.ext.commands import Cog
+from discord import (
+    Interaction,
+    Member,
+    Colour,
+    Embed,
+)
+
+
+class Interaction(Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @command(name="gmorning", description="Greet someone good morning!")
+    async def gmorning(self, interaction: Interaction, member: Member):
+        if member is None:
+            await interaction.response.send_message(
+                content="No member provided",
+                delete_after=5,
+                ephemeral=True
+            )
+
+            return
+
+        await interaction.response.send_message(
+            content=f"Hey {member.mention}, " \
+                    f"{interaction.message.author.mention}" \
+                    f"said good morning ☀.",
+        )

bd522706b2297946846af1fb394b763569310bbf
RexcellentRawr <rexcellent.rawr@gmail.com>
2023-02-05 01:03:15 +0800

    update rawr cog to use command prefix

--- src/features/general.py
+++ src/features/general.py
@@ -1,22 +1,18 @@
-from discord.app_commands.commands import command
+from discord.ext.commands import command
 from discord.ext.commands import Cog
-from discord import (
-    Interaction,
-    Colour,
-    Embed,
-)
+from discord import Colour, Embed
 
 
 class Rawr(Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @command(name="commands", description="Shows all commands")
-    async def commands(self, interaction: Interaction):
+    @command()
+    async def commands(self, ctx):
         embed_help = Embed(
             title="Commands",
             description="Here are the commands available to use",
             colour=Colour.yellow()
         )
 
-        await interaction.response.send_message(embed=embed_help)
+        await ctx.send(embed=embed_help)

56a7f280609b8ee2581c570a230fc228ff173dfa
RexcellentRawr <rexcellent.rawr@gmail.com>
2023-02-04 18:50:18 +0800

    add giphy defaults

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,2 +1,11 @@
 # BOT RELATED
 CREATED_BY = "Made with 🥨 by Rex 🦖"
+
+# GIPHY RELATED
+BASE = "https://api.giphy.com/v1/gifs/search?"
+API = "api_key="
+Q = "&q="
+LIMIT = "&limit=25"
+OFFSET = "&offset=5"
+RATING = "&rating=g"
+LANG = "&lang=en"

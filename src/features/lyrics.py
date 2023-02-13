89bcb20995d77c47682507f7c9fe270d3b381729
RexcellentRawr <rexcellent.rawr@gmail.com>
2023-02-13 11:10:31 +0800

    add lyrics cog

--- src/features/lyrics.py
+++ src/features/lyrics.py
@@ -1,32 +1,64 @@
+import os
+
 from discord.ext.commands import Cog
 from discord.ext.commands import command
 from discord import Embed, Colour
 
-from azlyrics.azlyrics import lyrics
-
 from src.utils.constants import *
+
+from lyricsgenius import Genius
 
 
 class Lyrics(Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.genius = Genius(os.environ.get("GENIUS_TOKEN"))
 
     @command()
     async def lyrics(self, ctx, *song):
-        song_data = " ".join(song).split("-")
-        song_artist = song_data[0].replace(" ", "")
-        song_title = song_data[1].replace(" ", "")
-        song_lyrics = ""
+        if len(song) <= 1:
+            await ctx.send(
+                content="Follow the format `.lyrics <song-artist> - <song-title>`",
+                delete_after=10,
+                ephemeral=True
+            )
 
-        for line in lyrics(song_artist, song_title):
-            song_lyrics += line
+        song_data = " ".join(song).split("-")
+        song_artist = song_data[0].strip().title()
+        song_title = song_data[1].strip().title()
+        song = self.genius.search_song(song_title, song_artist)
 
         embed_lyrics = Embed(
-            title=song_data[1].strip().title(),
-            description=song_lyrics,
+            title=song_title,
+            description=song.lyrics,
             colour=Colour.orange()
         )
+
         embed_lyrics.set_author(name=song_data[0].strip().title())
         embed_lyrics.set_footer(text=AZFOOTER)
 
+        print(song.lyrics)
+        print(song)
+
         await ctx.send(embed=embed_lyrics)
+
+    #
+    # @command()
+    # async def lyrics(self, ctx, *song):
+    #     song_data = " ".join(song).split("-")
+    #     song_artist = song_data[0].replace(" ", "")
+    #     song_title = song_data[1].replace(" ", "")
+    #     song_lyrics = ""
+    #
+    #     for line in lyrics(song_artist, song_title):
+    #         song_lyrics += line
+    #
+    #     embed_lyrics = Embed(
+    #         title=song_data[1].strip().title(),
+    #         description=song_lyrics,
+    #         colour=Colour.orange()
+    #     )
+    #     embed_lyrics.set_author(name=song_data[0].strip().title())
+    #     embed_lyrics.set_footer(text=AZFOOTER)
+    #
+    #     await ctx.send(embed=embed_lyrics)
